4a8af68307250e512f712e3e124835e3c74873f9
nimarahnamadev1996 <nimarahnamadev1996@gmail.com>
2025-04-09 12:17:28 +0330

    adding clerk and pages

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+
+
+const Header = () => {
+  return (
+    <header className='bg-gradient-to-r from-blue-300 to-purple-400 shadow-lg'>
+
+        <div className='flex justify-between items-center p-3 max-w-6xl mx-auto '>
+
+            <Link 
+             href='/'
+             className='text-2xl font-extrabold group cursor-pointer'>
+
+             <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700 drop-shadow-md group-hover:from-blue-400 group-hover:to-blue-600'>Auth</span>
+
+             <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-purple-700 drop-shadow-md group-hover:from-purple-400 group-hover:to-purple-600'>
+              app
+             </span>
+            </Link>
+
+            <nav>
+                <ul className='flex gap-4'>
+                  <Link href='/'>Home</Link>
+                  <Link href='/about'>About</Link>
+                  <SignedIn>
+                     <UserButton />
+                  </SignedIn>
+
+                  <SignedOut>
+                   <SignInButton />
+                  </SignedOut>
+                </ul>
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Header
